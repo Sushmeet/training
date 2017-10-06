34cb66f7eeafd197ff06691a12ccd2152bc072a8
sushmeet <sushmeet.sunger@gmail.com>
2017-10-06 09:38:24 -0400

    created grades and users function

--- asyncAwait/udemycoursestuff/app-promises.js
+++ asyncAwait/udemycoursestuff/app-promises.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+
 const users = [
   {
     id: 1,
@@ -11,25 +12,62 @@ const users = [
     schoolId: 202
   }
 ];
-const grades = [];
+const grades = [
+  {
+    id: 1,
+    schoolId: 101,
+    grade: 86
+  },
+  {
+    id: 2,
+    schoolId: 202,
+    grade: 100
+  },
+  {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+  }
+];
 
 // function getUsers takes in id.
-//function returns a promise back to us.
-const getUsers = (id) => {
+// function returns a promise back to us.
+const getUsers = id => {
   return new Promise((resolve, reject) => {
-    const user = users.find((user) =>  user.id === id);
+    const user = users.find(user => user.id === id);
+    
     if (user) {
-        resolve(user);
-    }
-    else {
-        reject('error has occured');
+      resolve(user);
+    } else {
+      reject("error has occured");
     }
   });
 };
-getUsers(3)
-  .then(user => {
-    console.log(user);
+
+const getGrades = schoolId => {
+  return new Promise((resolve, reject) => {
+    const grade = grades.filter(grade => grade.schoolId === schoolId);
+
+    if (grade) {
+      resolve(grade);
+    } else {
+      reject("no grade is found");
+    }
+  });
+};
+
+// getUsers(1)
+//   .then(user => {
+//     console.log(user);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+getGrades(101)
+  .then(grade => {
+    console.log(grade);
   })
-  .catch(err => {
-    console.log(err);
+  .catch(e => {
+    console.log(e);
   });
