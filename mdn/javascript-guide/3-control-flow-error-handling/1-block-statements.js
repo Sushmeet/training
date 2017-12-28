deb0a45c76aa8e1fed794fe33ccb67abebe47037
sushmeet <sushmeet.sunger@gmail.com>
2017-12-28 15:24:39 -0500

    block statment

---
+++ mdn/javascript-guide/3-control-flow-error-handling/1-block-statements.js
@@ -0,0 +1,20 @@
+// block statment is used to group
+//  A BUNCH OF statements
+
+/* eg { statement_1;
+        statement_2;
+        statement_n;
+    }
+*/
+
+const willy = () => {
+    let x = 0;
+    while(x < 10) {
+        x++;
+        console.log (x);
+    } // x++ is the block statment
+}
+
+willy();
+
+
