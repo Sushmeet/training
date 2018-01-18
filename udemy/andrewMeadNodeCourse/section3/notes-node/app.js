a9596e2ec7c74533bf737720c84025433755dd00
sushmeet <sushmeet.sunger@gmail.com>
2018-01-18 17:56:12 -0500

    exporting notes module

--- udemy/andrewMeadNodeCourse/section3/notes-node/app.js
+++ udemy/andrewMeadNodeCourse/section3/notes-node/app.js
@@ -1,10 +1,11 @@
+console.log('starting app.js');
 // core npm modules.
 // fs file system we use the file append method.
 // os we use the user info method.
 
 // how we can use modules
 // require is a function in node
-// it will let us load modules which are 
+// it will let us load modules which are
 // bundled with node
 // http modules which let us make http requests.
 // fs module which lets us access the file system
@@ -12,11 +13,21 @@
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes");
 
 let user = os.userInfo();
-console.log(user);
+// console.log(user); // user object if u want to see
+/*
+{ uid: 501,
+  gid: 20,
+  username: 'ssunger',
+  homedir: '/Users/ssunger',
+  shell: '/bin/bash' }
+*/
 
 // append items to a file
 fs.appendFile("greetings.txt", ` Hello ${user.username}!`, err => {
   if (err) throw err;
 });
+
+
