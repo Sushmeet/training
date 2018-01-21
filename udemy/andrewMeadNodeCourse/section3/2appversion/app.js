a82ad1aa2061f1d5c11cda7d332e0485ad2d12cb
sushmeet <sushmeet.sunger@gmail.com>
2018-01-21 11:37:14 -0500

    check second argument

--- udemy/andrewMeadNodeCourse/section3/2appversion/app.js
+++ udemy/andrewMeadNodeCourse/section3/2appversion/app.js
@@ -2,25 +2,26 @@ console.log("Starting app.js");
 
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require('yargs');
 
 const notes = require("./notes");
 
-const command = process.argv[2].toLowerCase();
-console.log("Command: ", command);
-console.log(process.argv);
+if (process.argv[2]) {
+  const command = process.argv[2].toLowerCase();
+  console.log("Command: ", command);
+  console.log(process.argv);
 
-if (command === "add") {
-  console.log("Adding all notes");
-} else if (command === "list") {
-  console.log("Listing all notes");
-} else if (command === "read") {
-  console.log("Reading all notes");
-} else if (command === "remove") {
-  console.log("Removing Note");
-} else {
-  console.log("command not found");
+  if (command === "add") {
+    console.log("Adding all notes");
+  } else if (command === "list") {
+    console.log("Listing all notes");
+  } else if (command === "read") {
+    console.log("Reading all notes");
+  } else if (command === "remove") {
+    console.log("Removing Note");
+  } else {
+    console.log("command not found");
+  }
 }
-
-
 
 
