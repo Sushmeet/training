02301ea43938545f15f9654ee2da4c21ec5b6e04
sushmeet <sushmeet.sunger@gmail.com>
2018-01-20 22:42:29 -0500

    using command line argument in application

---
+++ udemy/andrewMeadNodeCourse/section3/2appversion/app.js
@@ -0,0 +1,26 @@
+console.log("Starting app.js");
+
+const fs = require("fs");
+const _ = require("lodash");
+
+const notes = require("./notes");
+
+const command = process.argv[2].toLowerCase();
+console.log("Command: ", command);
+console.log(process.argv);
+
+if (command === "add") {
+  console.log("Adding all notes");
+} else if (command === "list") {
+  console.log("Listing all notes");
+} else if (command === "read") {
+  console.log("Reading all notes");
+} else if (command === "remove") {
+  console.log("Removing Note");
+} else {
+  console.log("command not found");
+}
+
+
+
+
