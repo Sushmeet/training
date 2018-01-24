6d2d511639dee50ab6b9f49f4131cf9ba411df27
sushi <sushmeet sunger>
2018-01-23 22:29:00 -0500

    hmm

--- udemy/andrewMeadNodeCourse/section3/2appversion/notes.js
+++ udemy/andrewMeadNodeCourse/section3/2appversion/notes.js
@@ -3,31 +3,32 @@ console.log('Starting notes.js\n');
 const fs = require('fs');
 
 const addNote = (title, body) => {
-    let notes = []
-    const originalNote = {
-        title: title,
-        body: body,
+    let notes = [];
+    const note = {
+        title,
+        body
     }
 
+    try {
+        const originalFileString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(originalFileString); // returns back an array.
+    } catch (e) {}
 
-    const originalFileString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(originalFileString);
-    //notes.push(JSON.parse(originalFileString));
-    console.log('OLD Notes Array', notes);
 
-    if (title && body) {
-        notes.push(originalNote);
-    console.log('New Notes array', notes);
+    const duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (title && body && duplicateNotes.length === 0) {
+        notes.push(note);
         fs.writeFileSync('notes-data.json', JSON.stringify(notes));
     }
 }
 
 const getAll = () => {
     console.log('Getting all notes \n');
-   const notesString = fs.readFileSync('Notes.json');
-   const note = JSON.parse(notesString);
-   let arr = [];
-   arr = note;
+    const notesString = fs.readFileSync('Notes.json');
+    const note = JSON.parse(notesString);
+    let arr = [];
+    arr = note;
     console.log(arr[0]);
 }
 
