702775c4095c9c92bfd71b629931828513eb2783
sushmeet <sushmeet.sunger@gmail.com>
2018-01-30 16:31:55 -0500

    debugger

---
+++ udemy/andrewMeadNodeCourse/section3/playground/debugging.js
@@ -0,0 +1,12 @@
+
+// node inspect debugging.js ( to run it)
+
+let person = {
+    name: 'john',
+}
+
+person.age = 29;
+debugger; // allows u to debug applications
+person.name = "cassandra";
+
+console.log(person);
