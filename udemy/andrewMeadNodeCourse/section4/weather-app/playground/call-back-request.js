0d0b48338846f9de4d640f766b3abb683ca04e45
sushmeet <sushmeet.sunger@gmail.com>
2018-01-31 18:10:32 -0500

    request strigifyed

--- udemy/andrewMeadNodeCourse/section4/weather-app/playground/call-back-request.js
+++ udemy/andrewMeadNodeCourse/section4/weather-app/playground/call-back-request.js
@@ -1,8 +1,12 @@
-const request = require('request');
+const request = require("request");
 
-request.get({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=232%20bergevin%20montreal%20quebec',
-    json:true
-}, (error, response, body) => {
-    console.log(body);
-})
+request.get(
+  {
+    url:
+      "https://maps.googleapis.com/maps/api/geocode/json?address=232%20bergevin%20montreal%20quebec",
+    json: true
+  },
+  (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2));
+  }
+);
