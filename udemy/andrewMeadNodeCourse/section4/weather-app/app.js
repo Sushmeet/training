0bd0b5b0027e085d2647f3b655f884303025a35e
sushmeet <sushmeet.sunger@gmail.com>
2018-02-02 19:02:39 -0500

    weather app calling weather api

---
+++ udemy/andrewMeadNodeCourse/section4/weather-app/app.js
@@ -0,0 +1,28 @@
+const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      String: true
+    }
+  })
+  .help()
+  .alias("help", "-h").argv;
+
+
+const encodedurl = encodeURIComponent(argv.a);
+const decodedurl = decodeURIComponent(encodedurl);
+
+request.get(
+  {
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${decodedurl}`,
+    json: true
+  },
+  (error, response, body) => {
+    console.log(JSON.stringify(body, undefined, 2));
+  }
+);
