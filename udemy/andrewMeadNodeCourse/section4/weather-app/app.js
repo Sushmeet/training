01f5e73d79423a275aeecfd32ac6c68841f596ae
sushmeet <sushmeet.sunger@gmail.com>
2018-02-05 11:20:11 -0500

    should update error handling

--- udemy/andrewMeadNodeCourse/section4/weather-app/app.js
+++ udemy/andrewMeadNodeCourse/section4/weather-app/app.js
@@ -13,19 +13,26 @@ const argv = yargs
   .help()
   .alias("help", "-h").argv;
 
-
 const encodedurl = encodeURIComponent(argv.a);
 const decodedurl = decodeURIComponent(encodedurl);
 
 request.get(
   {
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${decodedurl}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedurl}`,
     json: true
   },
   (error, response, body) => {
     //console.log(JSON.stringify(body, undefined, 2));
-    console.log(`Formatted address ${body.results[0].formatted_address}`);
-    console.log(`Latitude ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude ${body.results[0].geometry.location.lng}`);
+    if (error) {
+      console.log("Error connection to google servers", error.code);
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log(`no results found for ${argv.a}`);
+    } else if (body.status === "OVER_QUERY_LIMIT") {
+      console.log(body.error_message);
+    } else if (body.status === "OK") {
+      console.log(`Formatted address ${body.results[0].formatted_address}`);
+      console.log(`Latitude ${body.results[0].geometry.location.lat}`);
+      console.log(`Latitude ${body.results[0].geometry.location.lng}`);
+    }
   }
 );
