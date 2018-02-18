e3bf444980b6a7dc8e7c018597dd0e28c9919629
sushi <sushmeet sunger>
2018-02-18 02:47:30 -0500

    promise2

--- udemy/andrewMeadNodeCourse/section4/weather-app/playground/promise2.js
+++ udemy/andrewMeadNodeCourse/section4/weather-app/playground/promise2.js
@@ -1,25 +1,18 @@
-const asyncAdd = (a, b) =>
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (typeof a === "number" && typeof b === "number") {
-        resolve(a + b);
-      } else {
-        reject(`${a} and ${b} are not both numbers. Try again `);
-      }
-    }, 1000);
-  });
+const asyncAdd = (a, b) => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (typeof a === "number" && typeof b === "number") {
+      resolve(a + b);
+    } else {
+      reject(`${a} and ${b} are not both numbers. Try again `);
+    }
+  }, 1000);
+});
 
-asyncAdd(2, 3)
-  .then(res => {
-    console.log(res);
-    return asyncAdd(res, 33)
-  })
-  .then((res) => {
-      console.log(res);
-  })
-  .catch(err => {
-    console.log(err);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+asyncAdd(5, 7).then(res => {
+  console.log('Result', res);
+  return asyncAdd(res, 33)
+}).then((res) => {
+  console.log('Should be 45', res);
+}).catch(err => {
+  console.log(err);
+})
