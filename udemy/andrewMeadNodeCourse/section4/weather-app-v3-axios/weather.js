214aa0020b546a23fedd211dab9854d7ebcbc0cd
sushmeet sunger <sushmeet.sunger@gmail.com>
2018-02-18 23:03:03 +0000

    done

--- udemy/andrewMeadNodeCourse/section4/weather-app-v3-axios/weather.js
+++ udemy/andrewMeadNodeCourse/section4/weather-app-v3-axios/weather.js
@@ -4,14 +4,14 @@ const getWeather = (lat, long) => {
     const apiKey = "5758563b4f9eb5fe93beb8d5ac6675cf";
     const url = `https://api.darksky.net/forecast/${apiKey}/${lat},${long}`;
 
-   return axios.get(url).then((response) => {
-            return {
-                 temp: response.data.currently.temperature,
-                 appTemp: response.data.currently.apparentTemperature
-                }
-        }).catch ((err) => {
-            return err;
-        })
+    return axios.get(url).then((response) => {
+        return {
+            temp: response.data.currently.temperature,
+            appTemp: response.data.currently.apparentTemperature
+        }
+    }).catch((err) => {
+        return err;
+    })
 }
 
 module.exports = {
