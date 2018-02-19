96dc0a6d80fd2daf7ea1c36b6c4ab1d7d5bf22cf
sushmeet sunger <sushmeet.sunger@gmail.com>
2018-02-19 01:39:05 +0000

    publc page

--- udemy/andrewMeadNodeCourse/section5/node-webserver/server.js
+++ udemy/andrewMeadNodeCourse/section5/node-webserver/server.js
@@ -2,11 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>hello</h1>');
+    res.send({
+        name: 'Sushi2',
+        likes: [
+            'ice',
+            'chalk',
+            ]
+    });
 });
 
-app.listen('0.0.0.0:8080', () => {
-    console.log('App has started');
+app.get('/about', (req, res) => {
+    res.send('about page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        error: 'there is an error'
+    })
+})
+
+
+app.listen(process.env.PORT || 3000, process.env.IP || 'localhost', () => {
+    console.log('App has started again');
 });
