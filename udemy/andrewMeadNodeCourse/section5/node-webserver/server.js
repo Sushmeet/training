8ed60682a65f6ea0be48daf4cc2975fe3234bab9
sushi <sushmeet sunger>
2018-02-24 00:42:14 -0500

    hbs stuff middleware

--- udemy/andrewMeadNodeCourse/section5/node-webserver/server.js
+++ udemy/andrewMeadNodeCourse/section5/node-webserver/server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const path = require('path')
 const hbs = require('hbs');
 const app = express();
+const fs = require('fs');
 
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
 app.set('view engine', 'hbs');
+
+// register middleware by .use method
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now} ${req.method} ${req.url}`;
+    console.log(`${now} ${req.method} ${req.url}`);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 
 app.get('/', (req, res) => {
 res.render('home-page.hbs', {
