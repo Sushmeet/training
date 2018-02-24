6f0a13364b1245d8b8053b294f30ddfae8a7b4f8
sushi <sushmeet sunger>
2018-02-24 13:09:39 -0500

    stuff

--- udemy/andrewMeadNodeCourse/section6/utils.spec.js
+++ udemy/andrewMeadNodeCourse/section6/utils.spec.js
@@ -3,6 +3,10 @@ const expect = require("chai").expect;
 
 
 // expect([2,3,4]) an array toInclude(2)
+// expect({name: 'job', age: 10}).toInclude({age: 10}) // so u only care about some properties
+
+// write test should verify first and last names are set
+// assert it includes firstName and lastName with proper values 
 
 describe("Utils Unit tests", () => {
   it("should add two numbers", () => {
--- udemy/andrewMeadNodeCourse/section6/utils/utils.js
+++ udemy/andrewMeadNodeCourse/section6/utils/utils.js
@@ -1,6 +1,14 @@
 const add = (a, b) =>  a + b;
 
+const setName = (user, fullName) => {
+    var names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+    return user;
+}
+
 
 module.exports = {
     add,
+    setName,
 }
