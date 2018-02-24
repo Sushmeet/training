635394ec31b79eb43897843e0431cd67e865fbe3
sushmeet <sushmeet.sunger@gmail.com>
2018-02-24 18:17:53 -0500

    asynch mocha testing

--- udemy/andrewMeadNodeCourse/section6/utils.spec.js
+++ udemy/andrewMeadNodeCourse/section6/utils.spec.js
@@ -1,16 +1,40 @@
-const { add } = require("./utils/utils");
+const { add, setName, asyncAdd } = require("./utils/utils");
 const expect = require("chai").expect;
-
 
 // expect([2,3,4]) an array toInclude(2)
 // expect({name: 'job', age: 10}).toInclude({age: 10}) // so u only care about some properties
 
 // write test should verify first and last names are set
-// assert it includes firstName and lastName with proper values 
+// assert it includes firstName and lastName with proper values
 
 describe("Utils Unit tests", () => {
   it("should add two numbers", () => {
-    expect(add(2, 3)).to.equal(5);
-    expect(add(2, -3)).to.equal(-1); // also check if its anumber.
+    expect(add(2, 3)).to.be.a("number").to.equal(5);
+    expect(add(2, -3)).to.be.a("number").to.equal(-1);
+  });
+
+  it("should add two numbers asynchronously", done => {
+    asyncAdd(4, 3, sum => {
+      expect(sum).to.be.a("number").to.equal(7);
+      done();
+    });
+  });
+
+  it("should set name that is passed", () => {
+    const user = {
+      age: 10,
+      id: 2234
+    };
+
+    const user2 = {
+      age: 10,
+      id: 2234,
+      firstName: "Johhny",
+      lastName: "Bravo"
+    };
+    const res = setName(user, "Johhny Bravo");
+    expect(setName({}, "Johhny Bravo")).to.be.an("object").to.include({ firstName: "Johhny" });
+    expect(setName(user, "Johhny Bravo")).to.be.an("object").to.include({ firstName: "Johhny" });
+    expect(user).to.equal(res); // its the same user object. strict equal works here
   });
 });
--- udemy/andrewMeadNodeCourse/section6/utils/utils.js
+++ udemy/andrewMeadNodeCourse/section6/utils/utils.js
@@ -1,14 +1,20 @@
-const add = (a, b) =>  a + b;
+const add = (a, b) => a + b;
+
+const asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback (a + b);
+    }, 1000);
+};
 
 const setName = (user, fullName) => {
-    var names = fullName.split(' ');
-    user.firstName = names[0];
-    user.lastName = names[1];
-    return user;
-}
-
+  var names = fullName.split(" ");
+  user.firstName = names[0];
+  user.lastName = names[1];
+  return user;
+};
 
 module.exports = {
-    add,
-    setName,
-}
+  add,
+  setName,
+  asyncAdd
+};
