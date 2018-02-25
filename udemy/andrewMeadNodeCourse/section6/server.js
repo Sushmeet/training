649612593e728012d821645358a85c2c4a9291c6
sushmeet <sushmeet.sunger@gmail.com>
2018-02-24 20:35:15 -0500

    more express tests

--- udemy/andrewMeadNodeCourse/section6/server.js
+++ udemy/andrewMeadNodeCourse/section6/server.js
@@ -1,23 +1,40 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  //res.status(200).send('help');
+  res.send("help");
+});
 
-app.get('/', (req, res) => {
-    //res.status(200).send('help');
-    res.send('help');
-})
+app.get("/noFound", (req, res) => {
+  res.status(404).send("Page not found");
+});
 
-app.get('/noFound', (req, res) => {
-    res.status(404).send('Page not found');
-})
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "tom",
+      age: 10
+    },
+    {
+      name: "john",
+      age: 4
+    },
+    {
+      name: "bob",
+      age: 7
+    }
+  ];
+  res.send(users);
+});
 
 // app.listen(3500, () => {
 //     console.log('App has started on 3500');
 // })
 
-app.listen(3500)
+app.listen(3500);
 
 module.exports = {
-    app
-}
+  app
+};
--- udemy/andrewMeadNodeCourse/section6/server.spec.js
+++ udemy/andrewMeadNodeCourse/section6/server.spec.js
@@ -5,18 +5,31 @@ const app = require("./server").app;
 
 describe("Express server testing", () => {
   it("should return the / page", done => {
-   request(app)
+    request(app)
       .get("/")
       .expect(200)
       .expect("help")
       .end(done);
   });
 
-  it('should return the page not found', (done) => {
-      request(app)
-      .get('/noFound')
+  it("should return the page not found", done => {
+    request(app)
+      .get("/noFound")
       .expect(404)
-      .expect('Page not found')
-      .end(done)
-  })
+      .expect("Page not found")
+      .end(done);
+  });
+
+  it("should return back the user sushi", done => {
+    request(app)
+      .get("/users")
+      .expect(200)
+      .expect(res => {
+        expect(res.body).to.deep.include({
+          name: "john",
+          age: 4
+        });
+      })
+      .end(done);
+  });
 });
