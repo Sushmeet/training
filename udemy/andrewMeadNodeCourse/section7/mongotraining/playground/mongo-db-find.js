8e2ba3db0c649e68f8373783473b8e28c9aa353d
sushi <sushmeet sunger>
2018-03-07 23:35:30 -0500

    hhh

--- udemy/andrewMeadNodeCourse/section7/mongotraining/playground/mongo-db-find.js
+++ udemy/andrewMeadNodeCourse/section7/mongotraining/playground/mongo-db-find.js
@@ -1,21 +1,19 @@
-const {MongoClient} = require('mongodb')
+const { MongoClient } = require('mongodb')
 const database = 'TodoApp';
 const collection = 'Users';
 
 MongoClient.connect(`mongodb://localhost:27017/${database}`, (err, client) => {
     if (err) {
         console.log(`Cannot connet to mongo db`, err)
-    }
-    else {
+    } else {
 
         const db = client.db(database);
 
-
-        db.collection(collection).find(/*{name: 'Bonny'}*/).toArray().then((docs) => {
+        db.collection(collection).find( /*{name: 'Bonny'}*/ ).toArray().then((docs) => {
             console.log(JSON.stringify(docs, undefined, 2));
 
         }, (err) => {
-            console.log('unable to get videos' , err);
+            console.log('unable to get videos', err);
         })
     }
 })
