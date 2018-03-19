d7cefe82c1ca760948d96d600213a213bfe8a12c
sushmeet <sushmeet.sunger@gmail.com>
2018-03-19 11:37:25 -0400

    jfljkj

--- udemy/andrewMeadNodeCourse/section7/mongotraining/playground/mongoose2-orm.js
+++ udemy/andrewMeadNodeCourse/section7/mongotraining/playground/mongoose2-orm.js
@@ -1,18 +1,17 @@
-const { mongoose , closeConn} = require('../server/db/mongoose');
-const { Todo } = require('../server/models/todo');
+const { mongoose, closeConn } = require("../server/db/mongoose");
+const { Todo } = require("../server/models/todo");
 
-const id = '5aadf8e6bb97ea638034cf29';
+const id = "5aadf8e6bb97ea638034cf29";
 
- Todo.find({}).then((res) => {
-    console.log(res);
-})
+Todo.find({}).then(res => {
+  console.log(res);
+});
 
-Todo.findOne().then((res) => {
-    console.log(res);
-})
+Todo.findOne().then(res => {
+  console.log(res);
+});
 
-
-Todo.findById(id).then((res) => {
-    console.log(res);
-    closeConn();
-})
+Todo.findById(id).then(res => {
+  console.log(res);
+  closeConn();
+});
