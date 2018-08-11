c264a2b1b02658b1dbb9e5177c60f3727d63ce33
sushi <sushmeet sunger>
2018-08-11 10:37:17 -0400

    reduce

--- udemy/stephengrider/ES6-Javascript/reduce.js
+++ udemy/stephengrider/ES6-Javascript/reduce.js
@@ -5,5 +5,32 @@ const arr = [10, 20, 30];
 const result = arr.reduce((sum, number) => {
   return sum + number;
 }, 0);
+// console.log(result);
 
-console.log(result);
+// --------------------------------------------------
+var desks = [
+  { type: "sitting" },
+  { type: "standing" },
+  { type: "sitting" },
+  { type: "sitting" },
+  { type: "standing" }
+];
+
+let sittingCount = 0;
+let standingCount = 0;
+
+var deskTypes = desks.reduce(
+  function(acc, desk ) {
+    if (desk.type === "sitting") {
+    //   acc = { sitting: ++sittingCount, standing: standingCount };
+      acc.sitting++;
+    } else if (desk.type === "standing") {
+    //   acc = { sitting: sittingCount, standing: ++standingCount };
+      acc.standing++;
+    }
+    console.log(acc);
+    return acc;
+
+  },
+  { sitting: 0, standing: 0 }
+);
---
+++ udemy/stephengrider/ES6-Javascript/reduce2.js
@@ -0,0 +1,11 @@
+
+var number = [1, 1, 2, 3, 4, 4];
+
+// give back [1,2,3,4];
+
+function unique(array) {
+//   array.reduce((acc, current) => {
+
+
+//   }, []);
+}
