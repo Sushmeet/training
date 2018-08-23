62c97f86206f6c3c3a1e9fedf8fbeafbc8690d13
sushmeet <sushmeet.sunger@gmail.com>
2018-08-23 15:41:43 -0400

    updates

--- udemy/stephengrider/ES6-Javascript/every-some.js
+++ udemy/stephengrider/ES6-Javascript/every-some.js
@@ -3,9 +3,9 @@ const numbers = [10, 20, 30];
 // every
 
 const everyResult = numbers.every(number => number >= 20);
-console.log("everyResult", everyResult);
+console.log("everyResult", everyResult); // returns a boolean value (FALSE)
 
 // -----------------------------------------------------
 
 const someResult = numbers.some(number => number >= 20);
-console.log("someResult", someResult);
+console.log("someResult", someResult); // returns a boolean value (TRUE)
--- udemy/stephengrider/ES6-Javascript/reduce2.js
+++ udemy/stephengrider/ES6-Javascript/reduce2.js
@@ -1,11 +1,18 @@
-
 var number = [1, 1, 2, 3, 4, 4];
 
 // give back [1,2,3,4];
 
 function unique(array) {
-//   array.reduce((acc, current) => {
+ return  array.reduce((acc, current) => {
+    let value = acc.find(elm => elm === current);
+    if (!value) {
+        acc.push(current);
+    }
+    return acc;
 
-
-//   }, []);
+  }, []);
 }
+
+let blah = unique(number);
+
+console.log(blah);
