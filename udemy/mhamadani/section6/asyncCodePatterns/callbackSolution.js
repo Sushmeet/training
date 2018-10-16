a68dc0c57248fbd32bef96937cfa3e4bbc1b00db
sushmeet <sushmeet.sunger@gmail.com>
2018-10-15 23:07:05 -0400

    callback cleaned up

--- udemy/mhamadani/section6/asyncCodePatterns/callbackSolution.js
+++ udemy/mhamadani/section6/asyncCodePatterns/callbackSolution.js
@@ -1,15 +1,42 @@
 // So in async code u have to deal with synchronocity.
 const getUser = (id, cb) => {
-    setTimeout(() => {
-        console.log('Reading a user from a database');
-        cb ({id: id, githubUsername: 'sushi'})
-    }, 1000);
+  setTimeout(() => {
+    console.log("Reading a user from a database");
+    cb({ id: id, githubUsername: "sushi" });
+  }, 1000);
+};
+
+const getRepositories = (username, cb) => {
+  setTimeout(() => {
+    console.log(`calling ${username} github API`);
+    cb(["repo1", "repo2", "repo3"]);
+  }, 1000);
+};
+
+const getCommits = (repo, cb) => {
+  setTimeout(() => {
+    console.log("Print out all commits");
+    cb("commits");
+  });
+};
+
+const displayCommits = (value) => {
+    console.log(value);
 }
 
+const displayRepoNames = (reponames) => {
+    console.log(reponames);
+    getCommits(reponames, displayCommits); // here passing the function reference.
+}
 
-console.log('Before');
- getUser(1, (user) => {
+const displayUser = (user) => {
     console.log(user);
-});
-console.log('user', user);
-console.log('After');
+    getRepositories(user.githubUsername, displayRepoNames);
+}
+
+// ---- ALL functions Declared Above.
+
+console.log("Before");
+getUser(1, displayUser);
+// console.log('user', user);
+console.log("After");
