a24e3d5c7d4b8bccadc019711686e36f8fa3ba4e
sushmeet <sushmeet.sunger@gmail.com>
2018-10-22 14:20:23 -0400

    correct ordering of the logging

--- udemy/mhamadani/sections4/index.js
+++ udemy/mhamadani/sections4/index.js
@@ -5,11 +5,12 @@ const morgan = require('morgan');
 const startUpDebugger = require('debug')('app:startUP');
 const dBDebugger = require('debug')('app:dB');
 
-const logger = require('./logger');
+const logger = require('./middleware/logger');
 const courses = require('./routes/courses');
 const homePage = require('./routes/homePage')
 const app = express();
 
+// view engine set up
 app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -19,27 +20,22 @@ const hostName = "localhost";
 
 console.log('name', config.name);
 
+app.use(express.json()); // req.body will be populated with json object
+app.use(express.urlencoded({ extended: true })); // key=value&key=value , It populates it with json object on req.body
+app.use(express.static('public')); // serves static pages in public directory.
+app.use(helmet());
+
 if(process.env.NODE_ENV === 'dev' || process.env.NODE_ENV === 'qa') {
   app.use(morgan('tiny'));
   startUpDebugger('Morgan Started');
 }
 
-
-app.use(express.json()); // req.body will be populated with json object
-app.use(express.urlencoded({ extended: true })); // key=value&key=value , It populates it with json object on req.body
-app.use(express.static('public'));
-app.use(helmet());
-
+app.use(logger);
 app.use('/api/courses/',courses);
 app.use('/',homePage);
 
-
-
-
 // some db work so then
 dBDebugger('Connecting to MYSQl DATABASE');
-
-app.use(logger);
 
 app.listen(8900, hostName, () => {
   console.log(`Server has Started on port ${PORT}`);
