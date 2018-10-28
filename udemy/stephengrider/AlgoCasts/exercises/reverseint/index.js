fc9f9e2c5b245394e29813f60f7ab0681a5b1d2f
sushmeet <sushmeet.sunger@gmail.com>
2018-10-28 12:07:43 -0400

    reverse an integer

--- udemy/stephengrider/AlgoCasts/exercises/reverseint/index.js
+++ udemy/stephengrider/AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,26 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const strArray= n.toString().split('');
+    const newArray = [];
+    let swap;
+    for(i = 0; i < strArray.length / 2; i++) {
+        swap = strArray[i];
+        newArray[i] = strArray[strArray.length - 1 - i]
+        newArray[strArray.length - 1 - i] = swap;
+    }
+
+    return parseInt(newArray.join('')) * Math.sign(n);
+}
+
+console.log(reverseInt(-90));
 
 module.exports = reverseInt;
+
+// solution 1
+// function reverseInt(n) {
+//     const reverse = n.toString().split('').reverse().join('')
+
+//     return parseInt(reverse) * Math.sign(n);
+// }
