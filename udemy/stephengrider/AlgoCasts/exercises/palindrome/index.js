8be980d66bbfa0204eae9c1f8d0443824caaf482
sushmeet <sushmeet.sunger@gmail.com>
2018-10-28 10:17:59 -0400

    pallindrome

--- udemy/stephengrider/AlgoCasts/exercises/palindrome/index.js
+++ udemy/stephengrider/AlgoCasts/exercises/palindrome/index.js
@@ -4,9 +4,43 @@
 // form the same word if it is reversed. *Do* include spaces
 // and punctuation in determining if the string is a palindrome.
 // --- Examples:
-//   palindrome("abba") === true
-//   palindrome("abcdefg") === false
+//   palindrome("abba") === true   a, ba, bba, abba
+//   palindrome("abcdefg") === false  a, ba, cba, dcba, edcba, fedcba, gfedcba
 
-function palindrome(str) {}
+function palindrome(str) {
+    let reverse = '';
+    for (const char of str) {
+         reverse = char + reverse
+    }
+    return str === reverse;
+}
 
+console.log(palindrome('abba'));
 module.exports = palindrome;
+
+// solution 1 
+// function palindrome(str) {
+    //     return str === str.split('').reverse().join('');
+    // }
+
+// solution 2 
+// function palindrome(str) {
+//     const reverseStr =  str.split('').reduce((acc, current) => {
+//          return current + acc;
+//      }, '')
+//      return str === reverseStr;
+//  }
+
+// solution 3 
+// function palindrome(str) {
+//     const strArray= str.split('');
+//     const newArray = [];
+//     let swap;
+//     for(i = 0; i < strArray.length / 2; i++) {
+//         swap = strArray[i];
+//         newArray[i] = strArray[strArray.length - 1 - i]
+//         newArray[strArray.length - 1 - i] = swap;
+//     }
+
+//     return str === newArray.join('');
+// }
