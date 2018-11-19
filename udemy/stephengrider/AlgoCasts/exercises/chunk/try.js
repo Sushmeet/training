86662dad24521b68feb6e3c88cd9e1c0d750510b
sushmeet <sushmeet.sunger@gmail.com>
2018-11-18 19:28:58 -0500

    try

---
+++ udemy/stephengrider/AlgoCasts/exercises/chunk/try.js
@@ -0,0 +1,16 @@
+function chunk(arr, splitNum) {
+    let chunkedArr = []
+    console.log(arrNew);
+
+    for(i = 0; i < arr.length; i++) {
+        let tempArr = []
+        for (j = 0; j < splitNum; i++) {
+            tempArr[i] = tempArr.push()
+        }
+    }
+
+}
+
+
+
+chunk([1, 2, 3, 4], 2);
