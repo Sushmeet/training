34e84e3b295c3dffd70f79d6a23c10affedfb24e
sushi <sushmeet sunger>
2018-12-09 22:46:32 -0500

    block stuff

--- books/Kyle/scopes-and-closures/block-let-scope.js
+++ books/Kyle/scopes-and-closures/block-let-scope.js
@@ -1,10 +1,21 @@
-a = 2;
-console.log('Good value for a ', a);
-var a // variable declaration get's compiled first so this code works.
+// a = 2;
+// console.log('Good value for a ', a);
+// var a // variable declaration get's compiled first so this code works.
 
 
-console.log('value for b', b); // will be undefined.
-var b = 2;
+// console.log('value for b', b); // will be undefined.
+// var b = 2;
 
 
- 
+ function hello(x, y) {
+    console.log(x, y);
+    return x + y
+ };
+
+ let obj = {hello};
+
+ console.log('initial obj', obj);
+ obj.health = 9
+ obj = hello(1,2);
+
+ console.log('after obj', obj);
