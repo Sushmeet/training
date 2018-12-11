f610115ae25a964917d56784f7dd5b7f3825e79e
sushmeet <sushmeet.sunger@gmail.com>
2018-12-10 20:57:10 -0500

    this

---
+++ books/Kyle/this-&-object-prototypes/trial1.js
@@ -0,0 +1,24 @@
+function identify() {
+    return this.name.toUpperCase();
+}
+
+function speak() {
+    var greeting = "Hello, I' m " + identify.call(this);
+    console.log(greeting); 
+}
+
+var me = {
+    name: 'Kyle'
+};
+
+var you = {
+    name: 'Reader'
+};
+
+identify.call(me); // Kyle
+identify.call(you); // Reader
+
+speak.call(me); // hello, i'm kyle
+speak.call(you); // helllo, i'm reader
+
+
