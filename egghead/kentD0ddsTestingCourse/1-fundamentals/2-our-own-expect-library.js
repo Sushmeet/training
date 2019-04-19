c4f9c0f5d611533333711cf49ceb102f08a1a757
sushmeet <sushmeet.sunger@gmail.com>
2019-04-18 22:06:39 -0400

    create our own expect library

---
+++ egghead/kentD0ddsTestingCourse/1-fundamentals/2-our-own-expect-library.js
@@ -0,0 +1,28 @@
+const sum = (x, y) => x + y;
+const difference = (x, y) => x - y;
+
+let result = sum(2, 3);
+let expected = 5;
+
+// if (result !== expected) {
+//   throw new Error(`${result} is not what we expected ${expected}`);
+// }
+expect(result).toBe(expected);
+
+result = difference(5, 3);
+expected = 2;
+
+expect(result).toBe(expected);
+
+function expect(actual) {
+  return {
+    toBe: expected => {
+      if (actual !== expected) {
+        throw new Error(`${result} is not what we expected ${expected}`);
+      } 
+      else {
+          return console.log(`Actual value ${actual} == Expected ${expected}`);
+      }
+    }
+  };
+}
