ec87a0853df30b6e5ee923d4ab4513cc7503db5f
sushmeet <sushmeet.sunger@gmail.com>
2019-04-20 14:33:38 -0400

    expectlibrary our own runner

---
+++ egghead/kentD0ddsTestingCourse/1-fundamentals/4-async-tests.js
@@ -0,0 +1,37 @@
+const sum = (x, y) => x + y;
+const difference = (x, y) => x - y;
+
+
+test('Add two Numbers', async () => {
+    let result = await sum(2, 3);
+    let expected = 5;
+  
+    expect(result).toBe(expected);
+})
+test('Subtract two Numbers', async () => {
+    result = await difference(5, 3);
+    expected = 2;
+    expect(result).toBe(expected);
+});
+
+ function test(title, callback) {
+  try {
+    callback();
+    console.log(`Sucess ${title}`);
+  } catch (err) {
+    console.error(`Fail ${err}`);
+    console.error(err);
+  }
+}
+
+function expect(actual) {
+  return {
+    toBe: expected => {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not what we expected ${expected}`);
+      } else {
+        return console.log(`Actual value ${actual} == Expected ${expected}`);
+      }
+    }
+  };
+}
