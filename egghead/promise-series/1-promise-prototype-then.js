5a6354985f45df5c443845054db723fde1dbf186
sushi <sushmeet sunger>
2019-06-10 22:36:54 -0400

    stuff

--- egghead/promise-series/1-promise-prototype-then.js
+++ egghead/promise-series/1-promise-prototype-then.js
@@ -1,8 +1,15 @@
-const fetch = require('node-fetch')
-
-const API_URL = "https://jsonplaceholder.typicode.com/todos/1"
+// const fetch = require('node-fetch')
+const API_URL = "https://jsonplaceholder.typicode.com/todos/1";
 
 const responsePromise = fetch(API_URL);
+console.log('first unresolved promise', responsePromise);
 
-responsePromise.then(res => res.json())
-.then(res => console.log(res));
+// console.log(responsePromise);
+
+responsePromise.then(res => {
+    const responseJson = res.json()
+  console.log('2 nd promise of json', responseJson);
+  return responseJson
+}).then((resJson) => {
+    console.log('finalresult JSON', resJson)
+})
