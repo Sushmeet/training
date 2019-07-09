d1b7626a13550e5bf561c5a09e78945a3073c53a
sushmeet <sushmeet.sunger@gmail.com>
2019-07-09 19:51:03 -0400

    finally

--- egghead/promise-series/3-promise-finally.js
+++ egghead/promise-series/3-promise-finally.js
@@ -1,5 +1,5 @@
 // const fetch = require('node-fetch')
-const API_URL = "https://swapi.co/api/films/";
+const API_URL = "https://swapi.co/api/films111/";
 const output = document.getElementById("output");
 const spinner = document.getElementById('spinner');
 
@@ -13,12 +13,18 @@ fetch(API_URL)
     })  
   .then(films => {
     output.innerText = getFilmTitles(films.results);
+    return films;
   })
   .catch(error => {
     console.log(error)
     output.innerText = ':('
+    return []; // return empty object in case of error.
   })
   .finally(() => spinner.remove())
+  .then((films) => {
+    // films is still availabel from the return on top.
+    console.log(films);
+  })
 
 function getFilmTitles(filmTitles) {
  return filmTitles
