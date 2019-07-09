3564959d849fc11d46fdc8b37c1b6804eeb43e5b
sushmeet <sushmeet.sunger@gmail.com>
2019-07-09 19:34:45 -0400

    stuff

--- jest/1.for-each-function.ts
+++ jest/1.for-each-function.ts
@@ -1,5 +1,4 @@
-
-function forEach222(items, callback) {
+function forEach222(items, callback: (items: any) => any) {
   for (let index = 0; index < items.length; index++) {
     callback(items[index]);
   };
