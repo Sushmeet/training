0f19168eacf92fcf6182232e2905ac4f9bb07cfb
sushmeet sunger <ssunger@shutterstock.com>
2020-03-24 16:41:02 -0400

    react stuff

---
+++ scrimba/react-bob-intro/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const MyApp = () => {
+ return ( <div>
+            <h1>Sushi</h1>
+            <p>I am fatiyaaa</p>
+            <ol>
+              <li>hello kitty</li>
+              <li>hello Joby</li>
+              <li>hello ramoboo</li>
+            </ol> 
+          </div>    
+        )
+}
+
+ReactDOM.render(< MyApp />, document.getElementById("root"))
