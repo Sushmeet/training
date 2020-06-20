8ce5cd2fdfc7cdadc3ab77a2928db41d161783d9
sushmeet sunger <ssunger@shutterstock.com>
2020-06-20 03:16:50 -0400

    changes

--- front-end-masters/will-sentence/functions-and-callbacks/callbacks-exercises.js
+++ front-end-masters/will-sentence/functions-and-callbacks/callbacks-exercises.js
@@ -4,26 +4,35 @@ console.log('Hello, world!');
 
 
 // Challenge 1
+// Create a function addTwo that accepts one input and adds 2 to it.
 function addTwo(num) {
-
+  return num + 2;
 }
 
 // To check if you've completed it, uncomment these console.logs!
-// console.log(addTwo(3));
-// console.log(addTwo(10));
+console.log(addTwo(3));
+console.log(addTwo(10));
 
 
 // Challenge 2
+// Create a function addS that accepts one input and adds an "s" to it.
 function addS(word) {
-
+return `s${word}`
 }
 
 // uncomment these to check your work
-// console.log(addS('pizza'));
-// console.log(addS('bagel'));
+console.log(addS('pizza'));
+console.log(addS('bagel'));
 
 
 // Challenge 3
+/*
+Create a function called map that takes two inputs:
+an array of numbers (a list of numbers) a 'callback' function - a function 
+that is applied to each element of the array (inside of the function 'map')
+Have map return a new array filled with numbers that are the result 
+of using the 'callback' function on each element of the input array.
+*/
 function map(array, callback) {
 
 }
