d98e69de44bd1a77306e4ad5b9e240ce827196ce
sushmeet sunger <ssunger@shutterstock.com>
2021-05-22 10:02:17 -0400

    allthis

---
+++ nextjs-blog/pages/materialui.js
@@ -0,0 +1,16 @@
+function MaterialUI() {
+  return (
+    <div className="container">
+      Welcome to Next.js!
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default MaterialUI;
