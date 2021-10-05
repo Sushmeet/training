f5081fd03cbb34bd5ecbcd3c7cda51971d3fe01b
sushmeet sunger <ssunger@shutterstock.com>
2021-10-05 13:12:55 -0400

    nextjs

--- front-end-masters/scott-moss/nextjs-beginner-course/pages/index.js
+++ front-end-masters/scott-moss/nextjs-beginner-course/pages/index.js
@@ -1,10 +1,18 @@
 // export default () => <h1>hello</h1>;
+import Link  from "next/link";
 
 const Home = () => {
   return (
     <>
       <h1>Home Page</h1>
       <p>welcome to the home page</p>
+      <Link href="/docs">
+        <a>go to docs</a>
+      </Link>
+      <hr></hr>
+      <Link href="/notes">
+        <a>go to notes</a>
+      </Link>
     </>
   );
 };
--- front-end-masters/scott-moss/nextjs-beginner-course/pages/notes/index.js
+++ front-end-masters/scott-moss/nextjs-beginner-course/pages/notes/index.js
@@ -1,3 +1,27 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../../components/styles.module.css";
+
+
+
 export default () => {
-  return <h1>Notes Page</h1>;
+  const router = useRouter();
+  return (
+    <>
+      <h1>Notes Page</h1>
+      <Link href="/notes/[id]" as={`/notes/1`}>
+        <a>Note 1</a>
+      </Link>
+      <hr></hr>
+      <div>
+        <button className={styles.button} onClick={() => router.push("/")}>
+          Go Home
+        </button>
+      </div>
+      <hr></hr>
+      <button onClick={() => router.push("/notes/[id]", `/notes/5`)}>
+        Click on note 5
+      </button>
+    </>
+  );
 };
