435557d9368b80859241a79cb5ee85974b07e49a
ssunger <ssunger@shutterstock.com>
2022-09-25 16:51:09 -0400

    install application dependencies

--- front-end-masters/scott-moss/full-stack-app/pages/_app.tsx
+++ front-end-masters/scott-moss/full-stack-app/pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import chakra provider, and extendtheme. we want to wrap our entire app in the chakra provider
+// this chakra provider will give every component encapsulated in it access to the context.Context is state that can be accessed
+// and shared. A provider just gives access to that state.
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// Most component libs follow conventions created , so create a theme, extend the theme, and then make the theme available by using a provider
+// so all the components in an application can access that theme.
 
-export default MyApp
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
