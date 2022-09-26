3069be9fed9dbf4c6f784eacbb005ca35cb0cdc6
ssunger <ssunger@shutterstock.com>
2022-09-26 00:28:53 -0400

    add some more laypout for sidebar, content and bottom player

--- front-end-masters/scott-moss/full-stack-app/components/playerLayout.tsx
+++ front-end-masters/scott-moss/full-stack-app/components/playerLayout.tsx
@@ -2,9 +2,16 @@ import { Box } from "@chakra-ui/layout";
 
 export const PlayerLayout = ({ children }) => {
   return (
-    <Box ml={3} mt={3}>
-      Layout Component
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top={0} left={0} width="250px" border="solid">
+        SideBar
+      </Box>
+      {/* Middle Children Content */}
+      <Box ml="250px">{children}</Box>
+      {/* bottom player */}
+      <Box position="absolute" bottom={0} left={0} border="solid">
+        Bottom Player
+      </Box>
     </Box>
   );
 };
