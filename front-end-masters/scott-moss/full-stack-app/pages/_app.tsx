f041c071fc2a2d75f24af924bbdd701e97d79323
ssunger <ssunger@shutterstock.com>
2022-09-25 23:26:25 -0400

    wrap the nextjs app in a chakra provider and add to the existing theme

--- front-end-masters/scott-moss/full-stack-app/pages/_app.tsx
+++ front-end-masters/scott-moss/full-stack-app/pages/_app.tsx
@@ -3,13 +3,46 @@
 // and shared. A provider just gives access to that state.
 
 // Most component libs follow conventions created , so create a theme, extend the theme, and then make the theme available by using a provider
-// so all the components in an application can access that theme.
+// so all the components in an application can access that theme. U can basically put ur on color pallette with chakra UI, so u can extend the theme
+//
 
-import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const colors = {
+    gray: {
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  };
+
+  const components = {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  };
+
+  const theme = extendTheme({ colors, components });
+  return (
+    <ChakraProvider theme={theme} resetCSS>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
