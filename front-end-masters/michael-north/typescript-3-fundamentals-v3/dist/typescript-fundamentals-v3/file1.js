70e2c5bdea5e2a6f5535d5581dc2bf54d7935bf6
ssunger <ssunger@shutterstock.com>
2023-01-28 14:04:46 -0500

    array tuples

--- front-end-masters/michael-north/typescript-3-fundamentals-v3/dist/typescript-fundamentals-v3/file1.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addNumbers = void 0;
-const addNumbers = (num1, num2) => {
-    return num1 + num2;
-};
-exports.addNumbers = addNumbers;
-const result = (0, exports.addNumbers)(2, 4);
-console.log("result----", result);
---
+++ front-end-masters/michael-north/typescript-3-fundamentals-v3/src/typescript-fundamentals-v3/3-3-Array-Tuples.ts
@@ -0,0 +1,17 @@
+// Tuple is a multi-element, ordered data structure where position of each item has
+// special meaning or convention. The kind of structure is often called a tuple.
+
+let myCar: [number, string, string] = [2002, "Toyota", "camry"];
+
+myCar = [9, "", ""];
+
+// Lets look at tuple restraints around lenght when we use push and pop
+// here type script doesn't protect u to add more elements to ur array.
+
+let yourCar: [number, number] = [1, 2];
+
+yourCar.push(3);
+yourCar.push(4);
+yourCar.push(5);
+
+console.log("yourCar----", yourCar);
