b25c486c386559a83191ccbb50b8fc13a8d0980a
ssunger <ssunger@shutterstock.com>
2023-01-27 19:11:28 -0500

    changes

--- front-end-masters/michael-north/typescript-3-fundamentals-v3/dist/tyy1.js
+++
@@ -1,3 +0,0 @@
-const add = (num1, num2) => {
-    return num1 + num2;
-};
---
+++ front-end-masters/michael-north/typescript-3-fundamentals-v3/src/typescript-fundamentals-v3/file1.ts
@@ -0,0 +1,7 @@
+export const addNumbers = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const result = addNumbers(2, 4);
+
+console.log("result----", result);
