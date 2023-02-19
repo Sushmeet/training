45be81ace95f23809171552c607fba5bc652cc7b
ssunger <ssunger@shutterstock.com>
2023-02-19 13:30:24 -0500

    intersection types

---
+++ front-end-masters/michael-north/typescript-3-fundamentals-v3/src/typescript-fundamentals-v3/3-5-Intersection-types.ts
@@ -0,0 +1,18 @@
+// Intersection conceptually is just an & operator.
+// so only overlap stuff
+
+const makeWeek = (): Date & { end: Date } => {
+  const firstdate = new Date();
+  const end = new Date();
+
+  return { ...firstdate, end };
+};
+
+const thisWeek = makeWeek();
+
+// This is quite different than what we saw with union types —
+// this is quite literally a Date and { end: Date } mashed together,
+// and we have access to everything immediately.
+
+thisWeek.end;
+thisWeek.getDate;
