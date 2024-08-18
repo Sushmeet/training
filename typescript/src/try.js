672e2ae9554219e72f37f24123ce359641399005
ssunger <ssunger@shutterstock.com>
2024-08-18 15:37:04 -0400

    stuff

--- typescript/src/try.js
+++ typescript/src/try.js
@@ -1,2 +1,9 @@
-// import express from 'express'
-// console.log('stuff', express);
+let obj = { name: "sushi", id: 123 };
+
+obj = { name: "joe", id: 2 };
+
+console.log("Obj---", obj);
+
+const cat = { name: "sushi", id: 123 };
+
+cat = { name: "joe", id: 2 };
