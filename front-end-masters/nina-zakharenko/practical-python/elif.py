cbff7cac1cf2ac619495364a68c7b2b1b0b3cc15
sylmyc <sylviemyc@gmail.com>
2024-09-22 12:16:26 -0400

    else if in python

---
+++ front-end-masters/nina-zakharenko/practical-python/elif.py
@@ -0,0 +1,12 @@
+def what_if(a):
+    if a >= 10:
+        print("num is greater than or equal 10")
+    elif a > 20:
+        print("num is greater than 20")
+    elif a > 30:
+        print("num is greater than 30")
+    else:
+        print("num is less than 10")
+
+
+what_if(10)
