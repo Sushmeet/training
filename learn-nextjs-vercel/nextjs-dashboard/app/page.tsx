354d28c7e86dd89b912cd6ccd9ef1e42b7f07bd1
sylmyc <sylviemyc@gmail.com>
2024-09-30 01:01:20 -0400

    Add a  desktop and Mobile here to the Home Page

--- learn-nextjs-vercel/nextjs-dashboard/app/page.tsx
+++ learn-nextjs-vercel/nextjs-dashboard/app/page.tsx
@@ -1,7 +1,8 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import styles from './ui/home.module.css';
+import Image from 'next/image';
+// import styles from './ui/home.module.css';
 import Link from 'next/link';
 
 export default function Page() {
@@ -12,7 +13,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <div className={styles.shape} />
+          {/* <div className={styles.shape} /> */}
           <div className="relative h-0 w-0 border-b-[26px] border-l-[15px] border-r-[15px] border-b-black border-l-transparent border-r-transparent" />
           <p
             className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className}`}
@@ -31,7 +32,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Desktop Hero Image  */}
+          <Image
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+          />
+          {/* Mobile hero Image below */}
+          <Image
+            src="/hero-mobile.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+            width={560}
+            height={620}
+            className="block md:hidden"
+          />
         </div>
       </div>
     </main>
