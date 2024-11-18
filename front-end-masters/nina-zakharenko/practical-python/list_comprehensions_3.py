7dc57a2c3728a8bf3b9b52c0ae66d59624eb7c77
ssunger <ssunger@shutterstock.com>
2024-11-18 00:43:32 -0500

    slicing

--- front-end-masters/nina-zakharenko/practical-python/list_comprehensions_3.py
+++ front-end-masters/nina-zakharenko/practical-python/list_comprehensions_3.py
@@ -6,5 +6,24 @@
 # (hint: try random.randint(min, max)). 
 # Finally, use a comprehension to create a set of every unique value from the above dictionary.
 
+# give me a list with numbers from 0 to 9
+list_of_numbers = [ num for num in range(0,100) if num % 2 == 0]
+print(f"list_of_numbers is {list_of_numbers}")
 
-#create a list of odd numbers 
+import random
+dic_example_2 = { num:random.randint(0,100) for num in list_of_numbers}
+print(f"dic_example_2 is {dic_example_2} and length is {len(dic_example_2)}")
+
+# create a dictonary which has key as name, and value as Bob
+dic_example = {"name": "Bob", "age": 30}, {"name": "Alice", "age": 25, "gender": "female"}
+print(f"dic_example is {dic_example} and len is {len(dic_example)}")
+
+
+set_dic_example_2 = { num for num in dic_example_2.values()}
+print(f"set_dic_example_2 is {set_dic_example_2} and len is {len(set_dic_example_2)}")
+
+
+
+
+
+
--- front-end-masters/nina-zakharenko/practical-python/slicing.py
+++ front-end-masters/nina-zakharenko/practical-python/slicing.py
@@ -21,3 +21,35 @@ my_new_string = my_string[:] # my_new_string is 'Hello, world!'
 
 #negative indexing.
 my_string[-1]
+
+
+
+# get last two characters
+my_list_sushi = ["h", "e", "l", "l", "o", "!"]
+print(f"my_list_sushi len is {len(my_list_sushi)}")
+answer = my_list_sushi[4:]
+print(f"answer is {answer}")
+
+
+# make a list of numbers from 0 to 100
+my_list_0_to_100 = [ num for num in range(0,101)]
+print(f"my_list_0_to_100 is {my_list_0_to_100}")
+
+# 
+my_list_0_to_100[30:70:2]
+print(f"my_list_0_to_100[30:70:2] is {my_list_0_to_100[30:70:2]}")
+
+my_list_0_to_100_reverse = my_list_0_to_100[::-1]
+print(f"my_list_0_to_100_reverse is {my_list_0_to_100_reverse}")
+
+
+# sliing time
+import random
+names = ["Nina", "Max", "Floyd", "Lloyd"]
+scores = [ random.randint(0,100) for _ in names]
+print(f"names is {names}")
+print(f"scores is {scores}")
+
+names.pop(3)
+for name, score in zip(names, scores):
+    print(f"Name is {name} and score is {score}")
