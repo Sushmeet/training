8b68c17e977a3832100d69fb485ae67f6d04b365
Sushmeet <sushmeet.sunger@gmail.com>
2024-12-01 22:43:25 -0500

    python classes

---
+++ front-end-masters/nina-zakharenko/practical-python/class_practice_1.py
@@ -0,0 +1,25 @@
+class Car:
+    runs = True
+    carName = "toyota"
+
+    def start(self):
+        if self.runs == True:
+            print(f"{self.carName} is working {self.runs}")
+        elif self.runs == False:
+            print(f"{self.carName} is not working {self.runs}")
+        else :
+            print(f"i don't know what is going on")
+
+            
+my_car = Car()
+my_car.start()
+my_car.runs = False
+my_car.start()
+
+my_audi = Car()
+my_audi.start()
+
+my_honda = Car()
+my_honda.carName = "Honda" 
+
+Car.start(my_honda)
