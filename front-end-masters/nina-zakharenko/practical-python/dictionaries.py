5cdfbfcfe3bda6dbcc429280bc2ce7738c16a4e6
Sushmeet <sushmeet.sunger@gmail.com>
2025-05-19 10:20:57 -0400

    add some more dictionary examples.

--- front-end-masters/nina-zakharenko/practical-python/dictionaries.py
+++ front-end-masters/nina-zakharenko/practical-python/dictionaries.py
@@ -6,3 +6,22 @@ print(f'student_name is ', student_info["name"])
 student_info["name"] = "Ringo Star"
 print(f'student_name is ', student_info["name"])
 
+# Python-specific dictionary features
+# Using different types as keys
+mixed_dict = {
+    "string_key": "value",
+    42: "number as key",
+    (1, 2): "tuple as key"  # This isn't possible in JavaScript!
+}
+print("\nMixed key types:", mixed_dict)
+
+# Dictionary methods
+print("\nDictionary methods examples:")
+print("Keys:", student_info.keys())
+print("Values:", student_info.values())
+print("Items:", student_info.items())
+
+# Dictionary comprehension (Python-specific feature)
+squares = {x: x*x for x in range(5)}
+print("\nDictionary comprehension:", squares)
+
