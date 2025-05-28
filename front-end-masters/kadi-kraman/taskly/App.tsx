c58aaec3a126a7930471b705d4dafe7d16d419f0
Sushmeet <sushmeet.sunger@gmail.com>
2025-05-27 20:31:53 -0400

    Refactor App component to use theme colors and improve structure

--- front-end-masters/kadi-kraman/taskly/App.tsx
+++ front-end-masters/kadi-kraman/taskly/App.tsx
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './theme';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app SUSHI!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>
+          Open up App.tsx to start working on your app SUSHI!2
+        </Text>
+      </View>
+      {/* 
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -13,8 +19,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: theme.colorLightBlue,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    borderBottomColor: theme.colorSerulian,
+    borderBottomWidth: 1,
+  },
+  itemText: {
+    fontWeight: 'bold',
+    fontSize: 14,
   },
 });
---
+++ front-end-masters/kadi-kraman/taskly/theme.ts
@@ -0,0 +1,4 @@
+export const theme = {
+  colorLightBlue: 'lightblue',
+  colorSerulian: '#1a759f',
+};
